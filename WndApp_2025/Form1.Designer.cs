74ad30ac06c4bdb5b98c50a38630a632e87fcee8
sunwaresubramaniam <subramaniam.chandramohan@sunware.in>
2025-03-03 11:46:39 +0530

    Add project files.

---
+++ WndApp_2025/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WndApp_2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.cmdClear = new System.Windows.Forms.Button();
+            this.cmdFetch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtResult.Location = new System.Drawing.Point(0, 108);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(800, 342);
+            this.txtResult.TabIndex = 0;
+            this.txtResult.Text = "";
+            // 
+            // cmdClear
+            // 
+            this.cmdClear.Location = new System.Drawing.Point(639, 10);
+            this.cmdClear.Name = "cmdClear";
+            this.cmdClear.Size = new System.Drawing.Size(126, 42);
+            this.cmdClear.TabIndex = 1;
+            this.cmdClear.Text = "Clear";
+            this.cmdClear.UseVisualStyleBackColor = true;
+            this.cmdClear.Click += new System.EventHandler(this.cmdClear_Click);
+            // 
+            // cmdFetch
+            // 
+            this.cmdFetch.Location = new System.Drawing.Point(639, 58);
+            this.cmdFetch.Name = "cmdFetch";
+            this.cmdFetch.Size = new System.Drawing.Size(126, 42);
+            this.cmdFetch.TabIndex = 2;
+            this.cmdFetch.Text = "Go..";
+            this.cmdFetch.UseVisualStyleBackColor = true;
+            this.cmdFetch.Click += new System.EventHandler(this.cmdFetch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "API Url";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(28, 66);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(605, 26);
+            this.txtUrl.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdFetch);
+            this.Controls.Add(this.cmdClear);
+            this.Controls.Add(this.txtResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtResult;
+        private System.Windows.Forms.Button cmdClear;
+        private System.Windows.Forms.Button cmdFetch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+    }
+}
+
---
+++ WndApp_2025/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using MTLS_Cert_Lib;
+
+namespace WndApp_2025
+{
+    public partial class Form1 : Form
+    {
+
+        string clientCertificatePath;
+        string clientCertificatePassword;
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clientCertificatePath = @"C:/Subbu/MTLS/Cert/SunwareClientCertificate25.pfx";
+            clientCertificatePassword = "Test@123";
+        }
+
+        private void cmdClear_Click(object sender, EventArgs e)
+        {
+            this.txtResult.Clear();
+            this.txtUrl.Clear();
+        }
+
+        private void cmdFetch_Click(object sender, EventArgs e)
+        {
+            //https://localhost:7130/api/Country/GetAllCountries
+            //https://localhost:7130/api/Customer/GetAllCustomers
+            //https://localhost:7130/api/User/GetAllUsers
+
+            this.txtResult.Text = string.Empty;
+            this.txtResult.Text = "Loading.. Please Wait";
+            if (!string.IsNullOrEmpty(this.txtUrl.Text))
+            {
+                this.txtResult.Text = this.txtResult.Text + "\r\n  Load the Certificate";
+                try
+                {
+                    MTLS_Cert_Lib.MTLSClientCertificate.ClientCertificatePath = clientCertificatePath;
+                    MTLS_Cert_Lib.MTLSClientCertificate.ClientCertificatePassword = clientCertificatePassword;
+                    MTLS_Cert_Lib.MTLSClientCertificate.ClientCertificate_CacheKey = "Testsubbu";
+                    //X509Certificate2 certificate = new X509Certificate2(clientCertificatePath, clientCertificatePassword);
+
+
+                    MTLS_Cert_Lib.MTLSClientCertificate.LoadCertificate();
+                    this.txtResult.Text = this.txtResult.Text + "\r\n  Creating the HttpClient";
+                    //HttpClient client = clientCertificate.CreateHttpClientWithCertificate(certificate);
+                    HttpClient client = MTLS_Cert_Lib.MTLSClientCertificate.CreateHttpClientAutomatic();
+                    this.txtResult.Text = this.txtResult.Text + "\r\n  Creating the HttpClient Response";
+                    this.txtResult.Text = this.txtResult.Text + "\r\n  Calling HttpClient Response";
+                    HttpResponseMessage response = MTLS_Cert_Lib.MTLSClientCertificate.HttpClientGet(client, this.txtUrl.Text);
+                    this.txtResult.Text = this.txtResult.Text + "\r\n  Getting the Response";
+                    string responseContent = MTLS_Cert_Lib.MTLSClientCertificate.GetHttpResponseContent(response);
+                    this.txtResult.Text = responseContent;
+                }
+                catch(Exception ex)
+                {
+                    this.txtResult.Text = this.txtResult.Text + "\r\n  Failed to execute:"+ex.Message;
+                }
+            }
+            else
+            {
+                this.txtResult.Text = this.txtResult.Text + "\r\n  Enter the Server Url";
+            }
+        }
+    }
+}
